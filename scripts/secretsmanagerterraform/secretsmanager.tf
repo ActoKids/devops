5d43c466b79afc488eb088380b07207428727598
greystar <34604046+LyndonP@users.noreply.github.com>
2019-02-02 09:46:46 -0800

    Adding secrets manager terraform scripts

--- scripts/secretsmanagerterraform/secretsmanager.tf
+++ scripts/secretsmanagerterraform/secretsmanager.tf
@@ -1,13 +1,16 @@
+#terraform resource for {KMS key} in aws
 resource "aws_kms_key" "secret" {
   description         = "Key for secret ${var.name}"
   enable_key_rotation = true
 }
 
+#terraform resource for {KMS alias} in aws
 resource "aws_kms_alias" "secret" {
   name          = "alias/${var.name}"
   target_key_id = "${aws_kms_key.secret.key_id}"
 }
 
+#terraform resource for {secret} in aws
 resource "aws_secretsmanager_secret" "secret" {
   description         = "${var.description}"
   kms_key_id          = "${aws_kms_key.secret.key_id}"
@@ -19,6 +22,7 @@ resource "aws_secretsmanager_secret" "secret" {
   tags                = "${var.tags}"
 }
 
+#terraform resource for {secret version} in aws
 resource "aws_secretsmanager_secret_version" "secret" {
   lifecycle {
     ignore_changes = [
