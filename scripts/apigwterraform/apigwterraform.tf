3b62a86bb0faacd9dd18a8609cd5384e8a0b6d4d
greystar <34604046+LyndonP@users.noreply.github.com>
2019-02-02 09:48:30 -0800

    Adding API Gateway terraform scripts

--- scripts/apigwterraform/apigwterraform.tf
+++ scripts/apigwterraform/apigwterraform.tf
@@ -1,5 +1,4 @@
-#terraform resource for {API GW resource} in aws
-########### run-DeleMessagesById ###########
+#terraform resource for {API GW resource} in aws (run-DeleMessagesById)
 resource "aws_api_gateway_resource" "run-delete-messages-by-id" {
   rest_api_id = "${var.REST_API_ID}"
   parent_id   = "${var.PARENT_ID}"
