551b93fb64ccf56994e870cb95ded70bde3851b4
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-05 13:56:04 -0800

    Update api.tf

--- scripts/lambdaterraform/api.tf
+++ scripts/lambdaterraform/api.tf
@@ -1,7 +1,7 @@
-variable "api_function" {
+variable "api_development" {
   type = "string"
 }
 
 data "aws_lambda_function" "existing" {
-  api_function = "${var.api_function}"
+  api_function = "${var.api_development}"
 }
