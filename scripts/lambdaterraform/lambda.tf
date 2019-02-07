04fe22025c63b0d60476a57cd124bb9531af10ac
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-06 19:09:33 -0800

    Update lambda.tf

--- scripts/lambdaterraform/lambda.tf
+++ scripts/lambdaterraform/lambda.tf
@@ -3,23 +3,22 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region     = "${var.region}"
-}
-
-# terraform that calls a API lambda function, api_development.
-variable "api_development" {
-  type = "string"
-}
-
-data "aws_lambda_function" "existing" {
-  api_development = "${var.api_development}"
+    region = "us-east-1"
 }
 
 # terraform that calls a crawler lambda function, crawler_development.
-variable "crawler_development" {
-  type = "string"
+resource "aws_lambda_function" "crawler_development" {
+    function_name = "crawler_development"
+    runtime = "python3.6"
+    filename = "crawler.py"
+    description = "Crawler test function"
 }
 
-data "aws_lambda_function" "existing" {
-  crawler_development = "${var.crawler_development}"
+# terraform that calls a API lambda function, api_development.
+resource "aws_lambda_function" "api_development" {
+    function_name = "api_development"
+    runtime = "nodejs4.3"
+    filename = "test.zip"
+    source_code_hash = "${base64sha256(file("test.zip"))}"
+    description = "API test function"
 }
