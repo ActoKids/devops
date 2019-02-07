26a94854b2de1dff2d1e96c59c7dc3c34085be3e
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-06 19:55:20 -0800

    Update and rename lambda.tf to lambda_crawler.tf

--- scripts/lambdaterraform/lambda_crawler.tf
+++ scripts/lambdaterraform/lambda_crawler.tf
@@ -13,13 +13,5 @@ resource "aws_lambda_function" "crawler_development" {
     filename = "index.py"
     handler = "index.handler"
     description = "Crawler test function"
-}
-
-# terraform that calls a API lambda function, api_development.
-resource "aws_lambda_function" "api_development" {
-    function_name = "api_development"
-    runtime = "nodejs8.10"
-    filename = "index.js"
-    handler = "index.handler"
-    description = "API test function"
+    role = "${aws_iam_role.lambda_exec_role.arn}"
 }
