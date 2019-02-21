e0fc7d306e54a4077942faebe399458984623b8b
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-21 00:38:24 -0800

    Merged the variable.tf to lambda.tf

--- scripts/lambdaterraform/lambda.tf
+++ scripts/lambdaterraform/lambda.tf
@@ -1,4 +1,33 @@
 # terraform devops/script/lambdaterrraform/lambda.tf
+# Terraform Variables
+# required for AWS
+
+variable "access_key" {}
+variable "secret_key" {}
+# the variable sets the runtime and defaults it to US-East-1
+variable "region" {
+    default = "us-east-1"
+}
+variable "function_name" {
+  type = "string"
+}
+variable "role" {
+    default = "arn:aws:iam::061431082068:role/iam_for_lambda"
+}
+# the variable determines the runtime and defaults it to python 3.6
+variable "runtime" {
+    default = "python3.6"
+}
+variable "handler" {
+  default = "lambda.handler"
+}
+variable "environment" {
+    type = "string"
+}
+variable "team" {
+    type = "string"
+}
+
 # required credential information in AWS
 provider "aws" {
     access_key = "${var.access_key}"
@@ -9,15 +38,15 @@ provider "aws" {
 # terraform that creates a Lambda function
 resource "aws_lambda_function" "lambda_development" {
     function_name = "ad440-w19-lambda-${var.team}-${var.function_name}"
-    filename = "lambda_function"
+    filename = "lambda_function.py"
     runtime  = "${var.runtime}"
     handler  = "${var.handler}"
     role     = "${var.role}"
     environment {
         variables = {
     Environment  = "${var.environment}"          //DEV, PROD
-    Name         = "${var.function_name}"        //Function_name
+    Name         = "${var.function_name}"        //Function name
     Team         = "${var.team}"                 //API, UX, CRAWLER
     }
-    }
+}
 }
