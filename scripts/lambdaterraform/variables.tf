8d8811c6622fcd900cc453f08ed5bd431d3919dd
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-21 00:22:34 -0800

    Added default values and new inputs

--- scripts/lambdaterraform/variables.tf
+++ scripts/lambdaterraform/variables.tf
@@ -3,6 +3,26 @@
 # required for AWS
 variable "access_key" {}
 variable "secret_key" {}
+# the variable sets the runtime and defaults it to US-East-1
 variable "region" {
-    default = "us-west-2"
+    default = "us-east-1"
+}
+variable "function_name" {
+  type = "string"
+}
+variable "role" {
+    default = "arn:aws:iam::061431082068:role/iam_for_lambda"
+}
+# the variable determines the runtime and defaults it to python 3.6
+variable "runtime" {
+    default = "python3.6"
+}
+variable "handler" {
+  default = "lambda.handler"
+}
+variable "environment" {
+    type = "string"
+}
+variable "team" {
+    type = "string"
 }
