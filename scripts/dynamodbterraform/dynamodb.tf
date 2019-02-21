ad1433bd31c55bd1d052ee32eab03a1b61515e6c
greystar <34604046+LyndonP@users.noreply.github.com>
2019-02-20 16:09:29 -0800

    Add files via upload

--- scripts/dynamodbterraform/dynamodb.tf
+++ scripts/dynamodbterraform/dynamodb.tf
@@ -2,7 +2,8 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region     = "${var.region}"
+    region     = "us-west-1"
+    alias      = "us-west-1"
 }
 
 # Terraform instantiating a DynamoDB called Message.
@@ -17,5 +18,10 @@ resource "aws_dynamodb_table" "messages" {
 attribute {
     name = "ID"
     type = "S"
+  }
+ tags {
+    Name         = "${var.name}"        //Table name
+    Team         = "${var.teamname}"    //API, UX, CRAWLER
+    Environment  = "${var.environment}" //ONEBOX, DEV, PROD
   }
 }
