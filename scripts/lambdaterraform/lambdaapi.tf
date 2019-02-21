e05b97d5d6a44151d59e77f3cf8102669bad9913
jams2018 <38123178+jams2018@users.noreply.github.com>
2019-02-20 19:20:48 -0800

    Fixed an error and modified the code. It should create a lambda function and the name of the function is asked.

--- scripts/lambdaterraform/lambdaapi.tf
+++ scripts/lambdaterraform/lambdaapi.tf
@@ -1,4 +1,4 @@
-# terraform devops/script/lambdaterrraform/lamdbaapi.tf
+# terraform devops/script/lambdaterrraform/lamdbacrawler.tf
 # required credential information in AWS
 provider "aws" {
     access_key = "${var.access_key}"
@@ -6,12 +6,32 @@ provider "aws" {
     region     = "${var.region}"
 }
 
-# terraform that calls a API lambda function, api_development.
-resource "aws_lambda_function" "api_development" {
-    function_name = "api_development"
-    runtime = "nodejs8.10"
-    filename = "index.js"
-    handler = "index.handler"
-    description = "API test function"
-    role = "${aws_iam_role.lambda_exec_role.arn}"
+# terraform that calls a crawler lambda function, crawler_development.
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+variable "function_name" {
+  type = "string"
+}
+
+data "aws_lambda_function" "existing" {
+  function_name = "${var.function_name}"
 }
