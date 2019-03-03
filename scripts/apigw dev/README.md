0943c59315b15b0eff36ade5fcccca7be03d33d0
greystar <34604046+LyndonP@users.noreply.github.com>
2019-03-03 15:29:36 -0800

    Adding the scripts for Sprint 4
    The "Sprint4_DEV.tf" is the main file

---
+++ scripts/apigw dev/README.md
@@ -0,0 +1,3 @@
+Create CNAME in Route53
+Configure API GW to use the CNAME + SSL certificate
+Configure the API Gateway to point to Lambda
---
+++ scripts/apigw dev/variables.tf
@@ -0,0 +1,7 @@
+# terraform/variables.tf
+
+
+provider "aws" {
+  access_key = "AKIAIOR7433JXNDGOQKQ"
+  secret_key = "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"
+}
