9694ea7eff39ef180da8be73eab6046ba3f4f774
dashinay <dawit42001@gmail.com>
2019-03-06 15:18:05 +0500

    added cloudfront distrubution script and changed the script to redirect route 53(2edusite.com) to cloudfront(it is hard coded, needs to be changed)

--- automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
+++ automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json
 aws route53 change-resource-record-sets --hosted-zone-id Z3OK8WXAZJPC8H --change-batch file://upsert-record-r53.json
 
