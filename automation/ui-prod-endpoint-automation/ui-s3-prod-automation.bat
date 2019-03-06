03237d648ab0b24c586c23457d2e1a3e42b5d8e8
dashinay <dawit42001@yahoo.com>
2019-03-06 10:03:25 -0800

    updated the script and created a windows version with command line batch script

---
+++ automation/ui-prod-endpoint-automation/ui-s3-prod-automation.bat
@@ -0,0 +1,3 @@
+@windows prompt batch script for automation with CLI
+aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json
+aws route53 change-resource-record-sets --hosted-zone-id Z254GRV0BIP8AH --change-batch file://upsert-record-r53.json
--- automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
+++ automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-CF_DOMAIN_NAME=$(aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json --query DomainName --output text)
-#F_DOMAIN_NAME=d1mx7lg22jtdst.cloudfront.net
-echo $CF_DOMAIN_NAME
-aws route53 change-resource-record-sets --hosted-zone-id Z3OK8WXAZJPC8H --change-batch file://upsert-record-r53.json
+aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json
+aws route53 change-resource-record-sets --hosted-zone-id Z254GRV0BIP8AH --change-batch file://upsert-record-r53.json
 
