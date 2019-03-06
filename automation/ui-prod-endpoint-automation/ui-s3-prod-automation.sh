db535038bd5a1914d4a69e3f03fbb72e5f44c0d8
dashinay <dawit42001@gmail.com>
2019-03-06 20:21:50 +0500

    small changes

--- automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
+++ automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-#aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json
+CF_DOMAIN_NAME=$(aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json --query DomainName --output text)
+#F_DOMAIN_NAME=d1mx7lg22jtdst.cloudfront.net
+echo $CF_DOMAIN_NAME
 aws route53 change-resource-record-sets --hosted-zone-id Z3OK8WXAZJPC8H --change-batch file://upsert-record-r53.json
 
