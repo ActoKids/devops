d24ccddf5fe282d5ce7e2308c826f2268ec1e769
dashinay <dawit42001@gmail.com>
2019-03-06 11:10:51 -0800

    added aws configure and env variables

--- automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
+++ automation/ui-prod-endpoint-automation/ui-s3-prod-automation.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#enviromental variables
+ACTO_HOSTED_ZONE_ID=Z254GRV0BIP8AH
+#shell/bash script for automating of endpoint creation(2edusite.com and www.2edusite.com)
+#configures credentials for aws
+aws configure
+#creates an cloudfront distribution with attached SSL certificate
 aws cloudfront create-distribution --cli-input-json file://create-distribution-cloudfront.json
-aws route53 change-resource-record-sets --hosted-zone-id Z254GRV0BIP8AH --change-batch file://upsert-record-r53.json
+#creates an or updates existing record set in
+aws route53 change-resource-record-sets --hosted-zone-id $ACTO_HOSTED_ZONE_ID --change-batch file://upsert-record-r53.json
 
