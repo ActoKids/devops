4fd2380595c6b9704160ceb67a5e63859c4d5459
Anar <38123178+jams2018@users.noreply.github.com>
2019-03-06 18:08:31 -0800

    Added more parameters,environment and tags.

--- scripts/lambdaterraform/lambda.tf
+++ scripts/lambdaterraform/lambda.tf
@@ -1,31 +1,24 @@
 # terraform devops/script/lambdaterrraform/lambda.tf
-# Terraform Variables
-# required for AWS
 
-variable "access_key" {}
-variable "secret_key" {}
-# the variable sets the runtime and defaults it to US-East-1
-variable "region" {
-    default = "us-east-1"
+# a test resource role is created for the lambda function
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
-variable "function_name" {
-  type = "string"
-}
-variable "role" {
-    default = "arn:aws:iam::061431082068:role/iam_for_lambda"
-}
-# the variable determines the runtime and defaults it to python 3.6
-variable "runtime" {
-    default = "python3.6"
-}
-variable "handler" {
-  default = "lambda.handler"
-}
-variable "environment" {
-    type = "string"
-}
-variable "team" {
-    type = "string"
+EOF
 }
 
 # required credential information in AWS
@@ -35,18 +28,21 @@ provider "aws" {
     region     = "${var.region}"
 }
 
-# terraform that creates a Lambda function
-resource "aws_lambda_function" "lambda_development" {
+# terraform that creates a Lambda function with environment and tags
+resource "aws_lambda_function" "function_name" {
     function_name = "ad440-w19-lambda-${var.team}-${var.function_name}"
-    filename = "lambda_function.py"
+    filename = "${var.file_name}"
     runtime  = "${var.runtime}"
     handler  = "${var.handler}"
-    role     = "${var.role}"
+    source_code_hash = "${base64sha256(file("${var.file_name}"))}"
+    role             = "${aws_iam_role.iam_for_lambda.arn}"
     environment {
         variables = {
-    Environment  = "${var.environment}"          //DEV, PROD
-    Name         = "${var.function_name}"        //Function name
-    Team         = "${var.team}"                 //API, UX, CRAWLER
+    Environment  = "${var.environment}"   //DEV, PROD
     }
-}
+    }
+    tags {
+    Name  = "${var.function_name}"        //Function name
+    Team  = "${var.team}"                 //API, UI, CRAWLER
+    }
 }
